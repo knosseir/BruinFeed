889bb47c361f55b3f43791f3a8e900731359da14
knosseir <kimo.nosseir@gmail.com>
2017-07-20 16:23:33 +0200

    Used Jsoup to put all valid dining halls into a HashSet

--- app/src/main/java/com/example/admin/bruinfeed/MainActivity.java
+++ app/src/main/java/com/example/admin/bruinfeed/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.admin.bruinfeed;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    //ArrayList diningHalls = new ArrayList();
+    Set<Element> diningHalls = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +58,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setTimeString();
+
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        runner.execute("hello");
     }
 
     @Override
@@ -97,5 +120,42 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void setTimeString() {
+        String currentTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        TextView time = (TextView) findViewById(R.id.currentTime);
+        time.setText(currentTimeString);
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String url = "http://menu.dining.ucla.edu/Menus";
+                Document doc = Jsoup.connect(url).get();
+
+                String title = doc.title();
+                Log.d("title", title);
+
+                Elements links = doc.select("a[href]");
+
+                for (Element link : links) {
+                    Log.d("link titles", link.attr("href"));
+                    if (link.attr("href").contains("/Menus/")) {
+                        diningHalls.add(link);
+                    }
+                }
+                Log.d("number of dining halls", "size: " + diningHalls.size());
+
+            }
+
+            catch (IOException e) {
+                Log.e("error", "This should never happen");
+            }
+
+            return "sup";
+        }
     }
 }
